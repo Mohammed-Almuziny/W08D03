b6387bd1f6223c4e7d6c665a8ca4e1da0d37d568
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-30 13:25:16 +0300

    create authentication middleware

--- routers/routes/roles.js
+++ routers/routes/roles.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { createRole, getRole } = require("./../controllers/roles");
+const authentication = require("./../middlewares/authentication");
 
 const rolesRouter = express.Router();
 
 rolesRouter.post("/createRole", createRole);
-rolesRouter.get("/getRole", getRole);
+rolesRouter.get("/getRole", authentication, getRole);
 
 module.exports = rolesRouter;
