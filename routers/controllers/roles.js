3682bab2b0ce31c9e0d7fe228093fa69160963d2
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-30 21:08:31 +0300

    add try catch blocks to all controllers

--- routers/controllers/roles.js
+++ routers/controllers/roles.js
@@ -1,32 +1,40 @@
 const rolesModel = require("./../../db/models/roles");
 
 const createRole = (req, res) => {
-  const { role, permissions } = req.body;
+  try {
+    const { role, permissions } = req.body;
 
-  const newRole = new rolesModel({
-    role,
-    permissions,
-  });
-
-  newRole
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
+    const newRole = new rolesModel({
+      role,
+      permissions,
     });
+
+    newRole
+      .save()
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 const getRole = (req, res) => {
-  rolesModel
-    .find({})
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+  try {
+    rolesModel
+      .find({})
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 module.exports = { createRole, getRole };
--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -8,65 +8,76 @@ const SALT = Number(process.env.SALT);
 const SECRET = process.env.SECRETKEY;
 
 const register = async (req, res) => {
-  const { email, password, role } = req.body;
+  try {
+    const { email, password, role } = req.body;
 
-  const savedEmail = email.toLowerCase();
-  const savedPassword = await bcrypt.hash(password, SALT);
+    const savedEmail = email.toLowerCase();
+    const savedPassword = await bcrypt.hash(password, SALT);
 
-  const newUser = new usersModel({
-    email: savedEmail,
-    password: savedPassword,
-    role,
-  });
-
-  newUser
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
+    const newUser = new usersModel({
+      email: savedEmail,
+      password: savedPassword,
+      role,
     });
+
+    newUser
+      .save()
+      .then((result) => {
+        res.status(201).json(result);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 const logIn = (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const savedEmail = email.toLowerCase();
+    const savedEmail = email.toLowerCase();
 
-  usersModel
-    .findOne({ email: savedEmail })
-    .then(async (result) => {
-      if (result) {
-        if (result.email === savedEmail) {
-          const savedPassword = await bcrypt.compare(password, result.password);
+    usersModel
+      .findOne({ email: savedEmail })
+      .then(async (result) => {
+        if (result) {
+          if (result.email === savedEmail) {
+            const savedPassword = await bcrypt.compare(
+              password,
+              result.password
+            );
 
-          if (savedPassword) {
-            const payload = {
-              role: result.role,
-              id: result._id,
-            };
+            if (savedPassword) {
+              const payload = {
+                role: result.role,
+                id: result._id,
+              };
 
-            const options = {
-              expiresIn: "60m",
-            };
+              const options = {
+                expiresIn: "60m",
+              };
 
-            const token = await jwt.sign(payload, SECRET, options);
+              const token = await jwt.sign(payload, SECRET, options);
 
-            res.status(200).json({ result, token });
+              res.status(200).json({ result, token });
+            } else {
+              res.status(400).json("invalid email or password");
+            }
           } else {
             res.status(400).json("invalid email or password");
           }
         } else {
-          res.status(400).json("invalid email or password");
+          res.status(404).json("email dose not exist");
         }
-      } else {
-        res.status(404).json("email dose not exist");
-      }
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 const getAllUsers = (req, res) => {
