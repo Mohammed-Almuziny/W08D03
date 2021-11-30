90576f9fa726750d1eab0f70bd0c9226f0beb7f1
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-11-30 14:16:55 +0300

    complete authorization middleware

--- routers/routes/roles.js
+++ routers/routes/roles.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const { createRole, getRole } = require("./../controllers/roles");
 const authentication = require("./../middlewares/authentication");
+const authorization = require("./../middlewares/authorization");
 
 const rolesRouter = express.Router();
 
 rolesRouter.post("/createRole", createRole);
-rolesRouter.get("/getRole", authentication, getRole);
+rolesRouter.get("/getRole", authentication, authorization, getRole);
 
 module.exports = rolesRouter;
