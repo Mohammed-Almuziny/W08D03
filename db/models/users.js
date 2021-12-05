ca7aa83f6d3dc681f922312b9396c0c6535165b0
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-05 14:21:20 +0300

    change default role for register to user

--- db/models/users.js
+++ db/models/users.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const users = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: mongoose.Schema.Types.ObjectId, ref: "Roles" },
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Roles",
+    default: "61a4fa77caeed24e3975dd7b",
+  },
 });
 
 module.exports = mongoose.model("users", users);
